a84bbdcf72550f2d032078c5e06ecfa1e7e522cb
iamshubh22 <sgshubham03@gmail.com>
2016-10-18 14:40:12 +0530

    Update

--- app.py
+++ app.py
@@ -3,20 +3,19 @@ from flask import Flask, request
 import os
 import time
 
-
-token = "xoxp-91848767090-91832453094-91914233108-e4c6ccf83f763c7f3620afbcb167d362"
-sc = SlackClient(token)
-
-print(sc.api_call('api.test'))
-
-criclive_token = "1n4NyiVvw9EPmc1YtnzfLi3D"
-
 app = Flask(__name__)
+
+#token = "xoxp-91848767090-91832453094-91914233108-e4c6ccf83f763c7f3620afbcb167d362"
+#sc = SlackClient(token)
+#print(sc.api_call('api.test'))
+
+TOKEN = "1n4NyiVvw9EPmc1YtnzfLi3D"
+
 
 @app.route('/', methods=['POST'])
 def criclive():
-    text = request.values.get('token')
-    print(text)
+    token = request.values.get('token')
+    return(token)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
